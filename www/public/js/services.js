c9a90cb5a4422fd2a85af2e61427e307079be9d0
carlosofpersia <carloseduardophp@gmail.com>
2016-02-17 23:51:07 -0200

    Versionamento do primeiro commit do client luzicity-app

---
+++ www/public/js/services.js
@@ -0,0 +1,36 @@
+/* global angular, document, window */
+'use strict';
+
+angular.module('starter.services', [])
+
+.controller('AppService', function($scope, $ionicModal, $ionicPopover, $timeout) {
+    //
+})
+
+.controller('AuthGoogleService', function($scope, $http) {
+    //
+})
+
+.controller('AuthGoogleCallbackService', function($scope, $timeout, $stateParams, ionicMaterialInk) {
+    //
+})
+
+.controller('LoginService', function($scope, $timeout, $stateParams, ionicMaterialInk) {
+    //
+})
+
+.controller('FriendsService', function($scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
+    //
+})
+
+.controller('ProfileService', function($scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
+    //
+})
+
+.controller('ActivityService', function( $scope, $stateParams, $timeout, ionicMaterialMotion, ionicMaterialInk) {
+    //
+})
+
+.controller('GalleryService', function( $scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
+    //
+});
